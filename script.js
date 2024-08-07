54c219db4beb40d5b30d0297f95ad585ebe1430a
zwitsalbby <secondianakris@gmail.com>
2024-08-07 11:20:36 +0700

    Update script.js

--- script.js
+++ script.js
@@ -25,3 +25,12 @@ var countdownFunction = setInterval(function() {
         document.getElementById("countdown").innerHTML = "EXPIRED";
     }
 }, 1000);
+
+function toggleVisibility(id) {
+    var element = document.getElementById(id);
+    if (element.classList.contains('hidden')) {
+        element.classList.remove('hidden');
+    } else {
+        element.classList.add('hidden');
+    }
+}
