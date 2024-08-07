b029f2baa0be6c144a9bf2a17f72f69e9c6d82f4
zwitsalbby <secondianakris@gmail.com>
2024-08-07 16:18:27 +0700

    Update script.js
    update sore 7 agustus

--- script.js
+++ script.js
@@ -34,3 +34,47 @@ function toggleVisibility(id) {
         element.classList.add('hidden');
     }
 }
+
+// Save checkbox state
+function updateCheckbox(element) {
+    localStorage.setItem(element.id, element.checked);
+}
+
+// Load checkbox state
+function loadCheckboxState() {
+    var checkboxes = document.querySelectorAll('#todo-list input[type="checkbox"]');
+    checkboxes.forEach(function(checkbox) {
+        var checked = localStorage.getItem(checkbox.id);
+        if (checked !== null) {
+            checkbox.checked = (checked === 'true');
+        }
+    });
+}
+
+// Add new item to the to-do list
+function addItem() {
+    var newItem = document.getElementById('new-item').value;
+    if (newItem.trim() !== "") {
+        var ul = document.getElementById('todo-list');
+        var listItems = ul.getElementsByTagName('li');
+        if (listItems.length < 6) {
+            var li = document.createElement('li');
+            var input = document.createElement('input');
+            input.type = 'checkbox';
+            input.onchange = function() {
+                updateCheckbox(input);
+            };
+            li.appendChild(input);
+            li.appendChild(document.createTextNode(" " + newItem));
+            ul.appendChild(li);
+            document.getElementById('new-item').value = "";
+        } else {
+            alert("Maximum of 3 additional items allowed.");
+        }
+    }
+}
+
+// Load the checkbox state on page load
+window.onload = function() {
+    loadCheckboxState();
+};
